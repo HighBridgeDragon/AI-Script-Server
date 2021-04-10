41f67513892405ab47870db1eb2cd27e8ec6f316
Ryo Takahashi <r-takahashi@eyetech.jp>
2021-04-10 14:40:27 +0900

    SlackWebHookへの投稿機能を追加

--- script_generator.py
+++ script_generator.py
@@ -1,64 +1,95 @@
-import os 
 import argparse
-from dotenv import load_dotenv
+import json
+import os
+import random
+import time
+
 import pya3rt
 import requests
-import time
-import random
+from dotenv import load_dotenv
 from tqdm import trange
 
-load_dotenv(dotenv_path = r".env")
+load_dotenv(dotenv_path=r".env")
+
 
 def a3rt_response(text):
     api_key = os.getenv(r"A3RT_API_KEY")
     client = pya3rt.TalkClient(api_key)
     response = client.talk(text)
-    return ((response[r'results'])[0])[r'reply']
+    return ((response[r"results"])[0])[r"reply"]
+
 
 def noby_response(text):
     api_key = os.getenv(r"COTOGOTO_API_KEY")
     endpoint = os.getenv(r"COTOGOT_API_ENDPOINT")
 
-    payload = {r'text': text, r'app_key': api_key}
+    payload = {r"text": text, r"app_key": api_key}
     r = requests.get(endpoint, params=payload)
     data = r.json()
     return data[r"text"]
 
+
 def main(args: argparse.Namespace):
     query = args.query
     conversation_steps = args.conversation_steps
+    post_flag = args.post_to_webhook
     current_response = query
     current_speeker = r""
+    target_webhook_url = r""
     scripts = []
 
-    for cs_idx in trange(conversation_steps, desc = r"会話生成中..."):
+    tarker_a_url = os.getenv(r"TAKER_A_WEBHOOK_URL")
+    tarker_b_url = os.getenv(r"TAKER_B_WEBHOOK_URL")
+
+    for cs_idx in trange(conversation_steps, desc=r"会話生成中..."):
         if cs_idx % 2 == 0:
             current_response = noby_response(current_response)
             current_speeker = r"a3rt"
+            target_webhook_url = tarker_a_url
         else:
             current_response = a3rt_response(current_response)
             current_speeker = r"noby"
+            target_webhook_url = tarker_b_url
 
         scripts.append(
-            {r"speaker": current_speeker, r"dialogue": current_response})
+            {
+                r"speaker": current_speeker,
+                r"dialogue": current_response,
+                r"webhook": target_webhook_url,
+            }
+        )
 
     print(r"--台本--")
     print(r"クエリ: ", query)
+
+    if post_flag:
+        requests.post(tarker_b_url, data=json.dumps({r"text": query}))
+
     for item in scripts:
         print("{0}:{1}".format(item[r"speaker"], item[r"dialogue"]))
-        time.sleep(1.5 + (random.randint(0, 10) - 5)*0.1)
+
+        if post_flag:
+            requests.post(
+                item[r"webhook"], data=json.dumps({r"text": item[r"dialogue"]})
+            )
+        time.sleep(1.5 + (random.randint(0, 10) - 5) * 0.1)
+
 
 if __name__ == r"__main__":
     parser = argparse.ArgumentParser(
-        prog = r"Script Generator",
-        usage = r"python script-generator.py -q 文章",
-        add_help=True
+        prog=r"Script Generator",
+        usage=r"python script-generator.py -q 文章",
+        add_help=True,
     )
 
     parser.add_argument(r"-q", r"--query", help=r"会話の起点となる文章", type=str, required=True)
-    parser.add_argument(r"-cs", r"--conversation_steps", help=r"会話のやり取り数", type=int, default=5)
+    parser.add_argument(
+        r"-cs", r"--conversation_steps", help=r"会話のやり取り数", type=int, default=5
+    )
+    parser.add_argument(
+        r"-p", r"--post_to_webhook", help=r"webhookへ投稿するフラグ", action=r"store_true"
+    )
 
     args = parser.parse_args()
 
     main(args)
-
